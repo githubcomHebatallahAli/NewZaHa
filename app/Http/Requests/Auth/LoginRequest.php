a826f95b653e759b6ca31d8c22957574dcfa7f8d
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-04-12 08:59:33 +0200

    login request.php

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -3,9 +3,11 @@
 namespace App\Http\Requests\Auth;
 
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class LoginRequest extends FormRequest
 {
@@ -24,6 +26,15 @@ class LoginRequest extends FormRequest
         ];
     }
 
+    public function authenticate()
+    {
+        // استخدم Auth::attempt() للتحقق من بيانات تسجيل الدخول
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            // إذا لم ينجح التحقق، قم بإرجاع خطأ أو رسالة مناسبة
+            throw new AuthenticationException('Invalid credentials');
+        }
+    }
+    
     public function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(response()->json([
