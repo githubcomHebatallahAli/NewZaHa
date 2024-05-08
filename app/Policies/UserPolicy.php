221f375c870180028188aafd2703a653322fcd0a
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-05-08 18:32:23 +0200

    update user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,27 +9,19 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
-
-    // public function showAllOrders(User $user, User $requestedUser)
-    // {
-
-
-
-
-    // }
-
     public function showAllOrders(User $user, $id)
     {
-        // Check if the user is an admin
         if ($user->isAdmin == 1) {
-            // Admins can see all orders
             return true;
         }
 
-        // Regular users can see their own orders
+        if ($id instanceof User) {
+            $id = $id->id;
+        }
+
         return $user->id == $id;
     }
-    }
+}
 
 
 
