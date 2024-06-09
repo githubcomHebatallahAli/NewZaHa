f4e4a0000ce2a55d877dbe203215070410c8d65e
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-06-09 05:01:30 +0200

    update

--- app/Http/Requests/EmailVerificationRequest.php
+++ app/Http/Requests/EmailVerificationRequest.php
@@ -4,6 +4,9 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
 class EmailVerificationRequest extends FormRequest
 {
     /**
@@ -25,5 +28,14 @@ class EmailVerificationRequest extends FormRequest
             'email' => ['required','email','exists:users'],
             'otp' =>['requirde','max:6']
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
     }
 }
