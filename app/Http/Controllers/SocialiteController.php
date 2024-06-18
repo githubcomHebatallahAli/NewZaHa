0bccc335534ba7e7625838b821e3ca8b029bcec0
Hebatallah Ali <heba.111980.ma@gmail.com>
2024-06-18 04:42:22 +0200

    update

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -25,39 +25,36 @@ public function redirectToGoogle()
     public function handleGoogleCallback()
     {
         try {
-            // الحصول على بيانات المستخدم من Google
-            $user = Socialite::driver('google')->user();
 
-            // البحث عن المستخدم في قاعدة البيانات حيث يكون معرّف الاجتماعي هو نفس المعرّف الذي وفرته Google
+            $user = Socialite::driver('google')->user();
+ 
             $findUser = User::where('social_id', $user->id)->first();
 
             if ($findUser) {
-                // تسجيل دخول المستخدم
+
                 Auth::login($findUser);
 
-                // إعادة توجيه المستخدم إلى لوحة التحكم أو الصفحة الرئيسية
+
                 return redirect('https://zaha-script.vercel.app');
             } else {
-                // إنشاء بيانات المستخدم باستخدام معلومات حساب Google
+
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
                     'social_id' => $user->id,
                     'social_type' => 'google',
-                    // يُنصح باستخدام طريقة أكثر أمانًا لإنشاء كلمة المرور
-                    // 'password' => bcrypt('my-google'),
+
                 ]);
 
                 Auth::login($newUser);
 
-                // إعادة توجيه المستخدم إلى لوحة التحكم أو الصفحة الرئيسية
-                return redirect('https://zaha-script.vercel.app');
+                return redirect()->away('https://zaha-script.vercel.app');
             }
         } catch (Exception $e) {
-            // التعامل مع الخطأ وتسجيله
+
             Log::error('Error during Google callback: '.$e->getMessage());
 
-            // إعادة توجيه المستخدم إلى صفحة تسجيل الدخول مع رسالة خطأ
+
             return redirect('https://zaha-script.vercel.app/user/login')->with('error', 'حدث خطأ أثناء تسجيل الدخول باستخدام Google.');
         }
     }
